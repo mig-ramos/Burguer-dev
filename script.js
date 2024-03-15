e9d526b37c055931db78f1f58c28db6e607810e7
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-03-14 21:17:31 -0300

    feat: build remove items of the cart

--- script.js
+++ script.js
@@ -99,3 +99,38 @@ function updateCartModal() {
 
   cartCounter.innerHTML = cart.length;
 }
+
+// Função para remover o item do carrinho
+cartItemsContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name");
+
+    removeItemCart(name);
+  }
+});
+
+function removeItemCart(name) {
+  const index = cart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    const item = cart[index];
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+
+    cart.splice(index, 1);
+    updateCartModal();
+  }
+}
+
+addressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
